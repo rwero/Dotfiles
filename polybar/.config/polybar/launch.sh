4bdc610de035d9729028f2b26e1c3466a82bc2e3
Redouane Moussaoui <RedouaneMoussaoui@protonmail.com>
2024-06-01 23:41:38 +0100

    add center date module

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,3 +9,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar mybar &
 polybar mybar1 &
+polybar middle &
