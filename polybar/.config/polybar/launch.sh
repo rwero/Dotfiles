25ed64ed0be5f3d4b415cabd0c4b0f129dfc694d
Redouane Moussaoui <RedouaneMoussaoui@protonmail.com>
2024-06-14 15:23:19 +0100

    date module

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,6 +9,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar mybar &
 polybar middle &
+polybar date &
 polybar time &
 polybar battery &
 polybar network &
