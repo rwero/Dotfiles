3908a4e40cb191d7e94f4e749a5135194c147c8d
Redouane Moussaoui <RedouaneMoussaoui@protonmail.com>
2024-06-14 15:24:23 +0100

    tray module

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,8 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar &
-polybar middle &
 polybar bspwm &
 polybar date &
 polybar time &
@@ -17,3 +15,4 @@ polybar network &
 polybar brightness &
 polybar cpu &
 polybar volume &
+polybar tray &
